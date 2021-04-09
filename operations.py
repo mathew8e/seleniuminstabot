80659b8de58c70d983f48ff1356fcba55306997c
mathew8e <123kumatan@gmail.com>
2021-04-09 18:04:06 +0200

    adding getting instagram

--- operations.py
+++ operations.py
@@ -1,8 +1,16 @@
+ElementCookieAccept = "/html/body/div[2]/div/div/div/div[2]/button[1]"
+
 
 def operations(dr):
-    dr.get("https://cntr.click/7DRZr8N")
+    dr.get("https://www.instagram.com/")
 
-    print("ending...")
+    login_operation(dr)
+
     while 1:
         pass
+
+    print("ending...")
     dr.quit()
+
+def login_operation(dr):
+    dr.find_element_by_xpath(ElementCookieAccept).click()
