eeaddbe56537c24afd9d69b574d27274d611a4b8
mathew8e <123kumatan@gmail.com>
2021-04-09 17:33:17 +0200

    debug commit

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import sys
 
-dr = webdriver.Chrome()
+#check if a headless switch is
+chrome_options = Options()
+if 1 in range(-len(sys.argv), len(sys.argv)):
+    if sys.argv[1] == "--headless":
+        chrome_options.headless = True
+
+dr = webdriver.Chrome(options=chrome_options)
