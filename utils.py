62521a1c927a6d6bde00874791da7cead6d7e71e
mathew8e <123kumatan@gmail.com>
2021-04-10 09:25:02 +0200

    headless doesnt work

--- utils.py
+++ utils.py
@@ -18,3 +18,8 @@ def wait_until_load(dr, url):
     while url != get_current_url(dr):
         #this looks like shit but i dont know how to make it differently
         pass
+
+def wait():
+    #simple stalling the program
+    while 1:
+        pass
